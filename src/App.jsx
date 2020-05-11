653ec2f76d4798207ff98cb7ee218a0b81ee39a1
bluechiptony <tony5egwu@gmail.com>
2020-05-11 16:37:06 +0100

    auth route setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.scss";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Login, SetPassword, ForgotPassword } from "./components/Authentication/Authentication";
+import { Dashboard } from "./components/dashboard/Dashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Route path="/" exact component={Login} />
+        <Route path="/forgot-password" exact component={ForgotPassword} />
+        <Route path="/activate-account/:code" exact component={SetPassword} />
+        <Route path="/reset-password/:code" exact component={SetPassword} />
+        <Route path="/home" exact component={Dashboard} />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Authentication/Authentication.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export const Login = () => {
+  return (
+    <div>
+      <h1>Login Component</h1>
+    </div>
+  );
+};
+
+export const ForgotPassword = () => {
+  return (
+    <div>
+      <h1>Forgot password</h1>
+    </div>
+  );
+};
+
+export const SetPassword = () => {
+  return (
+    <div>
+      <h1>Set password</h1>
+    </div>
+  );
+};
+
+export const DisplayMessage = () => {
+  return (
+    <div>
+      <h1>Message</h1>
+    </div>
+  );
+};
